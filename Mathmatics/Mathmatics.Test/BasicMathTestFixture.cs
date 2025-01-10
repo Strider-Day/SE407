787f4a41844dd0e7ea451e0f97203cd6cfb32877
Strider-Day <ajrockwell@email.neit.edu>
2025-01-09 19:21:56 -0500

    Add project files.

---
+++ Mathmatics/Mathmatics.Test/BasicMathTestFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmatics.Test
+{
+    public class BasicMathTestFixture : IDisposable
+    {
+        private BasicMath _testObject;
+
+        public BasicMath TestObject { get => TestObject; }
+        public BasicMathTestFixture()
+        {
+            _testObject = new BasicMath();
+        }
+        public void Dispose() 
+        {
+
+        }
+    }
+}
