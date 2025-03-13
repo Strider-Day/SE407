c4d07977bbd08a2e3856ca2d0ab3170d1d969da9
Strider-Day <ajrockwell@email.neit.edu>
2025-03-13 19:44:03 -0400

    week 6

---
+++ BlockBuster_Week6/BlockBuster_Week6/BlockBuster.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using BlockBuster.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockBuster.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string[] _myColors;
+        private readonly string[] _favCities;
+        private readonly string[] _favHobbies;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _myColors = ["red", "green", "blue"];
+            _favCities = ["Orlando", "Richmond", "Boston", "Houston", "Seattle"];
+            _favHobbies = ["Golf", "Tennis", "Video Games", "Playing with Dog", "Reading Manga"];
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Colors()
+        {
+            ViewBag.MyColors = _myColors;
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            ViewBag.FavCities = _favCities;
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            ViewBag.FavCities = _favHobbies;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
